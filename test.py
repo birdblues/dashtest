7d122a30de35d78d9834e18509963c7d5e8370d4
dhseo <birdblues@mac.com>
2023-12-28 18:47:42 +0900

    add test2

--- test.py
+++ test.py
@@ -1,5 +1,23 @@
 import keyring
+import mojito
+import pprint
 
 api_key = keyring.get_password("irp_api_key", "birdblues")
 api_secret = keyring.get_password("irp_api_secret", "birdblues")
 
+key = api_key
+secret = api_secret
+acc_no = "64012548-29"
+mock = False
+
+broker = mojito.KoreaInvestment(
+    api_key=key,
+    api_secret=secret,
+    acc_no=acc_no,
+    mock=mock,
+    exchange='서울'
+)
+print(broker)
+
+balance = broker.fetch_balance()
+pprint.pprint(balance)
